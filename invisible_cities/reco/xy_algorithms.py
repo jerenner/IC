307b885898db7eb922c003499597bb2c6b97ddd5
Gonzalo <gonzaponte@gmail.com>
2017-04-17 14:36:25 +0200

    fixes

--- invisible_cities/reco/xy_algorithms.py
+++ invisible_cities/reco/xy_algorithms.py
@@ -11,13 +11,13 @@ def find_algorithm(algoname):
         raise ValueError("The algorithm <{}> does not exist".format(algoname))
 
 
-def barycenter(xs, ys, qs):
+def barycenter(xs, ys, qs, default=np.nan):
     q    = np.sum(qs)
     n    = len(qs)
-    x    = np.average(xs, weights=qs)
-    y    = np.average(qs, weights=qs)
-    xvar = np.sum(qs * (xs - x)**2) / (q - 1) if n else 0
-    yvar = np.sum(qs * (ys - y)**2) / (q - 1) if n else 0
+    x    = np.average(xs, weights=qs)         if n and q>0 else default
+    y    = np.average(ys, weights=qs)         if n and q>0 else default
+    xvar = np.sum(qs * (xs - x)**2) / (q - 1) if n and q>0 else default
+    yvar = np.sum(qs * (ys - y)**2) / (q - 1) if n and q>0 else default
 
     c    = Cluster(q, x, y, xvar**0.5, yvar**0.5, n)
     return [c]
