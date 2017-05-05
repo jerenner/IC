9989827949fd40074b667c79b006de30876ac95b
jerenner <jrenner@nextp12.ific.uv.es>
2017-05-05 15:42:29 +0200

    Adds corona algorithm

--- invisible_cities/reco/xy_algorithms.py
+++ invisible_cities/reco/xy_algorithms.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 
 from invisible_cities.reco.params import Cluster
+from invisible_cities.core.system_of_units_c import units
 
 def find_algorithm(algoname):
     if algoname in sys.modules[__name__].__dict__:
@@ -20,5 +21,34 @@ def barycenter(xs, ys, qs, default=np.nan):
     yvar = np.sum(qs * (ys - y)**2) / (q - 1) if n and q>0 else default
 
     c    = Cluster(q, x, y, xvar**0.5, yvar**0.5, n)
-    return [c]
+    return c
 
+
+def corona(xs, ys, qs, rmax=30*units.mm, T=10.0*units.pes):
+    """
+    rmax is the maximum radius of a cluster
+    T is the threshold for local maxima (this kwarg may be unnecessary)
+    returns a list of Clusters
+    """
+    c = []
+    xs = np.copy(xs)
+    ys = np.copy(ys)
+    qs = np.copy(qs)
+
+    # While there are more local maxima
+    while len(qs) > 0:
+        i_max = np.argmax(qs)    # SiPM with largest Q
+        if qs[i_max] < T: break  # largest Q remaining is negligible
+
+        # get SiPMs within rmax of SiPM with largest Q
+        dists = np.sqrt((xs - xs[i_max]) ** 2 + (ys - ys[i_max]) ** 2)
+        cluster = np.where(dists < rmax)[0]
+
+        # get barycenter of this cluster
+        c.append(barycenter(xs[cluster], ys[cluster], qs[cluster]))
+
+        xs = np.delete(xs, cluster) # delete the SiPMs
+        ys = np.delete(ys, cluster) # contributing to
+        qs = np.delete(qs, cluster) # this cluster
+
+    return c
